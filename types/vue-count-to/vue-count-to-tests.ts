a5bbc1a373cf58014f4c30841f3f32146d880cf8
xiaomi <391147815@qq.com>
2018-07-06 10:39:10 +0800

    add new type

---
+++ types/vue-count-to/vue-count-to-tests.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueCountTo from 'vue-count-to';
+
+new Vue({
+    el: '#app',
+    components: {
+        VueCountTo
+    },
+    template: `
+        # Test
+    `
+});
